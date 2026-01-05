87f3636544c551419f5a2f658109b7d56ec95bbc
Nishini@2002 <roshalanishini29@gmail.com>
2026-01-05 14:23:49 +0530

    first commit

---
+++ server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb';
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+connectDB()
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API Working');
+});
+
+app.listen(port, () => console.log('Server started on port ' + port));
